d507316db1dd74423776d6aab38162551fdf0085
Sam Decrock <sam.decrock@gmail.com>
2013-12-17 20:43:25 +0100

    added montage and composite

--- imagemagick.js
+++ imagemagick.js
@@ -193,4 +193,32 @@ exports.convert = function(args, timeout, callback) {
 exports.convert.path = 'convert';
 
 
+exports.montage = function(args, timeout, callback) {
+  var procopt = {encoding: 'binary'};
+  if (typeof timeout === 'function') {
+    callback = timeout;
+    timeout = 0;
+  } else if (typeof timeout !== 'number') {
+    timeout = 0;
+  }
+  if (timeout && (timeout = parseInt(timeout)) > 0 && !isNaN(timeout))
+    procopt.timeout = timeout;
+  return exec2(exports.montage.path, args, procopt, callback);
+}
+exports.montage.path = 'montage';
+
+
+exports.composite = function(args, timeout, callback) {
+  var procopt = {encoding: 'binary'};
+  if (typeof timeout === 'function') {
+    callback = timeout;
+    timeout = 0;
+  } else if (typeof timeout !== 'number') {
+    timeout = 0;
+  }
+  if (timeout && (timeout = parseInt(timeout)) > 0 && !isNaN(timeout))
+    procopt.timeout = timeout;
+  return exec2(exports.composite.path, args, procopt, callback);
+}
+exports.composite.path = 'composite';
 
